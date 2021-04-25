dfb5f87ea27cea99031d3f0c11114f282f51e5de
shelimber1919 <shelimber2012@gmail.com>
2021-04-24 23:55:57 -0600

    func random y crtl funcional para operar el DCIM

--- src/Paginas/Jsx/Cliente/VerClientes1.js
+++ src/Paginas/Jsx/Cliente/VerClientes1.js
@@ -3,13 +3,15 @@ SIEMPRE DEBE HEREDAR DE LA CLASE COMPONENT*/
 
 import React, { Component } from 'react';
 import NuevoCliente1 from './NuevoCliente1';
+import NuevoCliente2 from './NuevoCliente2'
 
 class verclientes1 extends Component{
     render(){
         return(
             <div>
                 <p>Uso de class based Components</p>
-                <NuevoCliente1 />
+
+                <NuevoCliente2 />
             </div>
         )
     }
